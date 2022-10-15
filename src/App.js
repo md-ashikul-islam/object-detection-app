cf3c80da9acbe5a3f32357816e0c55109848aa47
Md Ashikul Islam <md.ashikul.islam@g.bracu.ac.bd>
2022-10-15 23:59:03 +0600

    detection works in console

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import React, { useRef, useState, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 // 1. TODO - Import required model here
 // e.g. import * as tfmodel from "@tensorflow-models/tfmodel";
+
 import Webcam from "react-webcam";
 import "./App.css";
 // 2. TODO - Import drawing utility here
 // e.g. import { drawRect } from "./utilities";
-
+import * as cocossd from "@tensorflow-models/coco-ssd"
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
@@ -16,7 +17,7 @@ function App() {
   const runCoco = async () => {
     // 3. TODO - Load network 
     // e.g. const net = await cocossd.load();
-    
+    const net = await cocossd.load();
     //  Loop and detect hands
     setInterval(() => {
       detect(net);
@@ -44,7 +45,8 @@ function App() {
       canvasRef.current.height = videoHeight;
 
       // 4. TODO - Make Detections
-      // e.g. const obj = await net.detect(video);
+      const obj = await net.detect(video);
+      console.log(obj);
 
       // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
